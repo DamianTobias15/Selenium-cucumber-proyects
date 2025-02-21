cf56c0016160869320a36cd24829782a01e78158
Damian Tobias <erickdamian17@gmail.com>
2025-02-21 10:02:41 -0600

    FixReportManager21

--- my-java-project/src/test/java/com/example/steps/AppTest.java
+++ my-java-project/src/test/java/com/example/steps/AppTest.java
@@ -1,10 +1,14 @@
 package com.example.steps;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features", // Ruta a los archivos .feature
+    glue = "com.example.steps",              // Paquete donde están los step definitions
+    plugin = {"pretty", "html:target/cucumber-reports/cucumber.html"} // Plugins para reporting
+)
 public class AppTest {
-    @Test
-    public void testExample() {
-        assertTrue(true, "This test should pass.");
-    }
 }
